a173f3dd1a6fd20791ddde194509c9e40a1ef027
Thiago Pereira <iamthiago2@gmail.com>
2023-11-18 13:07:07 -0300

    Adicionado body parser

--- src/config/basic.js
+++ src/config/basic.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(express.json())
+app.use(bodyParser.json({ limit: '5mb' }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 module.exports = app
